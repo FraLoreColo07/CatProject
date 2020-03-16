285de323aca698f515e02eb9237f831352d4bf4d
FraLoreColo07 <57748531+FraLoreColo07@users.noreply.github.com>
2020-03-16 20:28:50 +0100

    aggiornamento file movimento
    creazione file per possibile movimento personaggi

---
+++ Codice_Programmazione/Sprite_Movimento/idea_movimento.cs
@@ -0,0 +1,23 @@
+public class ch_movement
+{
+    
+    public void ch_movement_UP(/*Posizione*/)
+    {
+        positionY -= 10;
+    }
+
+    public void ch_movement_DOWN(/*Posizione*/)
+    {
+        positionY += 10;
+    }
+
+    public void ch_movement_LEFT(/*Posizione*/)
+    {
+        positionX -= 10;
+    }
+
+    public void ch_movement_RIGHT(/*Posizione*/)
+    {
+        positionX += 10;
+    }
+}
